b49f263c11bc90b17b5c64317dcc133484501a40
Viorel Zota <viorel.zota@smartdata.solutions>
2024-08-02 15:29:27 +0300

    chore: add Package.swift file

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "DatePicker",
+    defaultLocalization: "en",
+    products: [
+        .library(
+            name: "DatePicker",
+            targets: ["DatePicker"]),
+    ],
+    dependencies: [
+        // Dependencies can be added here
+        .package(url: "https://github.com/viorel-sds/Extended.git", .branch("master")),
+    ],
+    targets: [
+        .target(
+            name: "DatePicker",
+            dependencies: ["Extended"],
+            path: "DatePicker/Classes"
+        ),
+        .testTarget(
+            name: "DatePickerTests",
+            dependencies: ["DatePicker"],
+            path: "DatePickerTests"
+        ),
+    ]
+)
